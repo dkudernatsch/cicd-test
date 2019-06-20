6e22e615a64dadcc6f0e5aaead5ff5fe7c0d8ea1
Daniel Kudernatsch <d.kudernatsch@outlook.com>
2019-06-20 21:36:21 +0200

    init

---
+++ src/test/java/at/technikum/if17b094/Tester.java
@@ -0,0 +1,37 @@
+package at.technikum.if17b094;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class Tester {
+
+    @Test
+    public void test1(){
+        assertTrue(true);
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void testMock(){
+        List i = Mockito.mock(List.class);
+        when(i.size()).thenReturn(1);
+        assertEquals(1, i.size());
+    }
+
+    @Test
+    @Tag("integration")
+    public void testIntegration(){
+        assertTrue(true);
+    }
+
+
+}
